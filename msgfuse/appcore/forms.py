5e9c7b67545d646828e5c91826b514f7294c25e0
Bill He <hezhenqian@gmail.com>
2011-10-02 16:35:54 -0400

    updated the validators

--- msgfuse/appcore/forms.py
+++ msgfuse/appcore/forms.py
@@ -1,15 +1,20 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 dateChoices = ('InitialDate','EndDate')
 ViewChoices = ('OpeningClicks','ClosingClicks')
 
+def validate_int(value):
+    if value < 0:
+        raise ValidationError(u'Please have an appropriate value for views')
+    
 class DateForm(forms.Form):
 	dateType = forms.CharField(widget=forms.Select(choices=dateChoices))
 	dateValue = forms.DateField()
 
 class ViewForm(forms.Form):
-	RequiredViews = forms.CharField(label="", max_length=8)
-	ClosingViews = forms.CharField(label="", max_length=8)
+	RequiredViews = forms.IntegerField(label="", validators = [validate_int])
+	ClosingViews = forms.IntegerField(label="", validators = [validate_int])
 	
 class MessageForm(forms.Form):
-	msgValue = forms.CharField(label="", widget=forms.Textarea, max_length=500)
+	msgValue = forms.CharField(label="", widget=forms.Textarea, max_length=500, error_messages={'invalid': 'Please have a Message'})
