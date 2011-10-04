2a177f875d01a5f199edd6b7c035360065fb8868
David Guo <david.guo@uwaterloo.ca>
2011-10-04 19:30:20 -0300

    ..

--- msgfuse/settings.py
+++ msgfuse/settings.py
@@ -12,9 +12,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'msgfusedb',                      # Or path to database file if using sqlite3.
-        'USER': 'mfadm',                      # Not used with sqlite3.
-        'PASSWORD': 'fusion123',                  # Not used with sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
